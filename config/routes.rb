4a8c69b5234988e9f76cbf206bf336d07910f4f8
AndyRama <andyramaroson@gmail.com>
2021-04-11 21:28:44 +0200

    add routes

--- config/routes.rb
+++ config/routes.rb
@@ -1,8 +1,8 @@
 Rails.application.routes.draw do
   devise_for :users
   
-resources :pages
   root to: 'pages#home'
+  get '/users' => 'admin#users', as: :users
   
   
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
